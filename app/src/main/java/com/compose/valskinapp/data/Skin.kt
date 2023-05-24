2604b97de115edfc73bc2416fed5bfc010fc44b5
NoFouL <irawanaufal29209@gmail.com>
2023-05-24 10:05:11 +0700

    Initial commit

---
+++ app/src/main/java/com/compose/valskinapp/data/Skin.kt
@@ -0,0 +1,9 @@
+package com.compose.valskinapp.data
+
+data class Skin(
+    val id: String,
+    val name: String,
+    val photoUrl: String,
+    val price: String,
+    val description: String
+)
---
+++ app/src/main/java/com/compose/valskinapp/data/SkinData.kt
@@ -0,0 +1,4 @@
+package com.compose.valskinapp.data
+
+object SkinData {
+}
